da3d1519d815141e09a430e49ad96135a72874e0
drazero <ardkokkel@outlook.com>
2020-02-14 00:06:08 +0100

    Added leaflet basemap for location

--- js/main.js
+++ js/main.js
@@ -1,3 +1,20 @@
 window.onload = () => {
+
     new WOW().init();
+
+    var mymap = L.map('mapid').setView([52.24, 4.426], 13);
+
+    var icon = L.icon({
+        iconUrl: '../img/icon.png',
+        iconSize: [25,41],
+      });
+
+        
+        let googleStreets = L.tileLayer('https://{s}.google.com/vt/lyrs=m&x={x}&y={y}&z={z}',{
+            //     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+            maxZoom: 20,
+    subdomains:['mt0','mt1','mt2','mt3']
+}).addTo(mymap);
+
+    var marker = L.marker([52.24, 4.426], {icon: icon}).addTo(mymap);
 }
